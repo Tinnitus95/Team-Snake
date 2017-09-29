1ff4d905f4e20ab8ed651b93e71e93e392ca1515
Tinnitus95 <31408430+Tinnitus95@users.noreply.github.com>
2017-09-29 12:59:31 +0200

    Change error message and prettied code

--- loginScript.js
+++ loginScript.js
@@ -5,18 +5,23 @@ const passWord =["123","456","789"];
 function check(form) { /*function to check userid & password*/
                /*the following code checkes whether the entered userid and password are matching*/
                for(let i = 0; i < 3; i++) {
-               if(form.username.value == userName[i]  && form.password.value == passWord[i]) {
-                  valid = true;
-                  break;
-               } else {
+                 if(form.username.value == userName[i]  && form.password.value == passWord[i]) {
+                   valid = true;
+                   break;
+                  }
+
+                 else {
                  valid = false;
-               }
-               }
-               if (valid) {
+                  }
+              }
+                if (valid) {
                  window.open('https://www.google.se/')/*opens the target page while Id & password matches*/
                  }
                else {
-                   alert("Error Password or Username")/*displays error message*/
+
+                   document.getElementById("Change").innerHTML = "Prova Igen";
+
+                   //alert("Error Password or Username")/*displays error message*/
                }
 
 
