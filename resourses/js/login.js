5e93f96b221081ab5a9c8538a70ba761d6f90409
Silvia02 <srj1213@gmail.com>
2017-10-03 17:04:58 +0200

    fixed login error

--- resourses/js/login.js
+++ resourses/js/login.js
@@ -1,28 +1,26 @@
 const user = [{
-        username: "silvia",
-        password: "silvia"
-    }],
+    username: "silvia",
+    password: "silvia"
+}]
 
-
-
-    function check(form) { /*function to check userid & password*/
-        /*the following code checkes whether the entered userid and password are matching*/
-        for (let i = 0; i < users.length; i++) {
-            if (form.username.value == users[i].username && form.password.value == users[i].password) {
-                valid = true;
-                break;
-            } else {
-                valid = false;
-            }
-        }
-        if (valid) {
-            window.open('/Users/silvia/Development/git/Projekt1/Team-Snake/main.html', '_self') /*opens the target page while Id & password matches*/
+function check(form) { /*function to check userid & password*/
+    /*the following code checkes whether the entered userid and password are matching*/
+    for (let i = 0; i < user.length; i++) {
+        if (form.username.value == user[i].username && form.password.value == user[i].password) {
+            valid = true;
+            break;
         } else {
-
-            document.getElementById("Change").innerHTML = "Prova Igen";
-
-            //alert("Error Password or Username")/*displays error message*/
+            valid = false;
         }
-
-
     }
+    if (valid) {
+        window.open('/Users/silvia/Development/git/Projekt1/Team-Snake/main.html', '_self') /*opens the target page while Id & password matches*/
+    } else {
+
+        document.getElementById("Change").innerHTML = "Prova Igen";
+
+        //alert("Error Password or Username")/*displays error message*/
+    }
+
+
+}
