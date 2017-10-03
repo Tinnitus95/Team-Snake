5647135546cf4040001b1a58567b388f7012e47d
EliasSannefur <elias.sannefur1@gmail.com>
2017-10-03 13:11:57 +0200

    change pictures

--- Snake/Javascript/kursSida.js
+++ Snake/Javascript/kursSida.js
@@ -5,7 +5,7 @@ function openCity(evt, cityName) {
     // Get all elements with class="tabcontent" and hide them
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
+       tabcontent[i].style.display = "none";
     }
 
     // Get all elements with class="tablinks" and remove the class "active"
