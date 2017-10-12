6b9ece818043c59b69fc63871b9ffe1252802618
Simonkyh <Simon.gribert@student.kyh.se>
2017-10-12 10:38:22 +0200

    Fixed an issue with dropdown

--- Snake/Javascript/dropdownmenu.js
+++ Snake/Javascript/dropdownmenu.js
@@ -2,13 +2,13 @@ function openDrop(evt, tabName) {
     // Declare all variables
     var i, dropcontent, droplinks;
 
-    // Get all elements with class="tabcontent" and hide them
+    // Get all elements with class="dropcontent" and hide them
     dropcontent = document.getElementsByClassName("dropcontent");
     for (i = 0; i < dropcontent.length; i++) {
        dropcontent[i].style.display = "none";
     }
 
-    // Get all elements with class="tablinks" and remove the class "active"
+    // Get all elements with class="droplinks" and remove the class "active"
     droplinks = document.getElementsByClassName("droplinks");
     for (i = 0; i < droplinks.length; i++) {
         droplinks[i].className = droplinks[i].className.replace(" active", "");
