13361036a6b2e2399219281fa3b76dac18e6372b
Natasha <natasha.bosnjak@student.kyh.se>
2017-10-20 11:13:37 +0200

    added assignment in"avslutade kurser" for a course review
    added javascript for coursereview

---
+++ Snake/Javascript/coursereview.js
@@ -0,0 +1,29 @@
+function myFunctionThree() {
+    
+    var nodes = document.querySelectorAll('.questions textarea');
+    
+    for (var i in nodes) {
+      
+      if(nodes[i].type == "textarea"){
+        if(nodes[i].value == ""){
+          alert('Lämna svar på alla frågorna');
+          nodes[i].focus();
+          return false;
+          }
+        }
+        
+}
+    window.alert("Tack för dina svar!")
+  document.getElementById("coursereview").style.display = "none";
+    
+    
+}
+
+function reset() {
+    document.getElementById("q1").reset();
+    document.getElementById("q2").reset();
+    document.getElementById("q3").reset();
+    document.getElementById("q4").reset();
+    document.getElementById("q5").reset();
+}
+
