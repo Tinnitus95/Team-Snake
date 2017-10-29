bb59942791c2426f1defc4d8f4816e709f2d88d2
Silvia02 <srj1213@gmail.com>
2017-10-29 17:17:58 +0100

    la till assignments

---
+++ resourses/js/assignment.js
@@ -0,0 +1,12 @@
+// SKapa en funktion för att ta emot filen
+// I den funktionen ska du hämta filens värde 
+// Du ska hämta selecorns väde
+// Efter filen är uppladdad ska du skicka en window.alert med filnamn och kurs
+
+function assignments() {
+    // bind variabel till id "kurslista"
+
+    // bind variable till id "upload"
+
+    // gör en window.alert med tack för + filnamn + i kurs + kurslista.value
+}
