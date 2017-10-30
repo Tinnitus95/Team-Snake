1dfa9514d39fbbd68c8993e6196ce898af81f252
Simonkyh <Simon.gribert@student.kyh.se>
2017-10-30 10:45:01 +0100

    fixed an issue with adding a task when selecting diffrent classes

--- Snake/Javascript/addtask.js
+++ Snake/Javascript/addtask.js
@@ -103,3 +103,108 @@ function addTasks() {
 	document.getElementById("ps-table").appendChild(addtr); 
 
 }
+function addTaskw() {
+	var addtr = document.createElement("TR");
+	var tdname = document.createElement("TD");
+	var tdstatus = document.createElement("TD");
+	var tdgrade = document.createElement("TD");
+	var tdgrade2 = document.createElement("SELECT");
+	var tdgradeop1 = document.createElement("OPTION");
+	var tdgradeop2 = document.createElement("OPTION");
+	var tdgradeop3 = document.createElement("OPTION");
+	var tdna = document.createElement("A");
+
+	var name = document.getElementById("tasknamew").value;
+	var status = document.getElementById("tasksw").value;
+
+	var tn = document.createTextNode(name);
+	var ts = document.createTextNode(status);
+
+	tdgradeop1.text = "IG";
+	tdgradeop2.text = "G";
+	tdgradeop3.text = "VG";
+	
+	tdgrade2.appendChild(tdgradeop1);
+	tdgrade2.appendChild(tdgradeop2);
+	tdgrade2.appendChild(tdgradeop3);
+	tdgrade.appendChild(tdgrade2);
+	tdna.appendChild(tn);
+	tdname.appendChild(tdna);
+	tdstatus.appendChild(ts);
+	addtr.appendChild(tdname);
+	addtr.appendChild(tdstatus);
+	addtr.appendChild(tdgrade);
+
+	document.getElementById("pr-tablew").appendChild(addtr); 
+
+}
+function addTaske() {
+	var addtr = document.createElement("TR");
+	var tdname = document.createElement("TD");
+	var tdstatus = document.createElement("TD");
+	var tdgrade = document.createElement("TD");
+	var tdgrade2 = document.createElement("SELECT");
+	var tdgradeop1 = document.createElement("OPTION");
+	var tdgradeop2 = document.createElement("OPTION");
+	var tdgradeop3 = document.createElement("OPTION");
+	var tdna = document.createElement("A");
+
+	var name = document.getElementById("tasknamee").value;
+	var status = document.getElementById("taskse").value;
+
+	var tn = document.createTextNode(name);
+	var ts = document.createTextNode(status);
+
+	tdgradeop1.text = "IG";
+	tdgradeop2.text = "G";
+	tdgradeop3.text = "VG";
+	
+	tdgrade2.appendChild(tdgradeop1);
+	tdgrade2.appendChild(tdgradeop2);
+	tdgrade2.appendChild(tdgradeop3);
+	tdgrade.appendChild(tdgrade2);
+	tdna.appendChild(tn);
+	tdname.appendChild(tdna);
+	tdstatus.appendChild(ts);
+	addtr.appendChild(tdname);
+	addtr.appendChild(tdstatus);
+	addtr.appendChild(tdgrade);
+
+	document.getElementById("ps-tablee").appendChild(addtr); 
+
+}
+function addTaskh() {
+	var addtr = document.createElement("TR");
+	var tdname = document.createElement("TD");
+	var tdstatus = document.createElement("TD");
+	var tdgrade = document.createElement("TD");
+	var tdgrade2 = document.createElement("SELECT");
+	var tdgradeop1 = document.createElement("OPTION");
+	var tdgradeop2 = document.createElement("OPTION");
+	var tdgradeop3 = document.createElement("OPTION");
+	var tdna = document.createElement("A");
+
+	var name = document.getElementById("tasknameh").value;
+	var status = document.getElementById("tasksh").value;
+
+	var tn = document.createTextNode(name);
+	var ts = document.createTextNode(status);
+
+	tdgradeop1.text = "IG";
+	tdgradeop2.text = "G";
+	tdgradeop3.text = "VG";
+	
+	tdgrade2.appendChild(tdgradeop1);
+	tdgrade2.appendChild(tdgradeop2);
+	tdgrade2.appendChild(tdgradeop3);
+	tdgrade.appendChild(tdgrade2);
+	tdna.appendChild(tn);
+	tdname.appendChild(tdna);
+	tdstatus.appendChild(ts);
+	addtr.appendChild(tdname);
+	addtr.appendChild(tdstatus);
+	addtr.appendChild(tdgrade);
+
+	document.getElementById("art-tableh").appendChild(addtr); 
+
+}
