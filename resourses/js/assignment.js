7437a4619f8ab091e5e5588c621f0870de191ab2
Silvia02 <srj1213@gmail.com>
2017-10-30 10:19:25 +0100

    assignment

--- resourses/js/assignment.js
+++ resourses/js/assignment.js
@@ -1,12 +1,16 @@
+function file() {
+    let f =
+}
 // SKapa en funktion för att ta emot filen
-// I den funktionen ska du hämta filens värde 
-// Du ska hämta selecorns väde
-// Efter filen är uppladdad ska du skicka en window.alert med filnamn och kurs
+// I den funktionen ska hämta filens värde 
+// hämta selecorns väde
+// Efter filen är uppladdad ska skicka en window.alert med filnamn och kurs
 
 function assignments() {
     // bind variabel till id "kurslista"
+    var l =
 
-    // bind variable till id "upload"
+        // bind variable till id "upload"
 
-    // gör en window.alert med tack för + filnamn + i kurs + kurslista.value
+        // gör en window.alert med tack för + filnamn + i kurs + kurslista.value
 }
