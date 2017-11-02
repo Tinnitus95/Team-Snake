ab9b6a64e2ac19090ee637163c7f2f4ac883c71a
Natasha <natasha.bosnjak@student.kyh.se>
2017-11-02 16:27:12 +0100

    fixed drag and drop flaws and errors

--- Snake/Javascript/dragdrop.js
+++ Snake/Javascript/dragdrop.js
@@ -1,5 +1,6 @@
 (function()
 {
+
 	if
 	(
 		!document.querySelectorAll 
@@ -9,6 +10,7 @@
 		window.opera
 	) 
 	{ return; }
+
 	for(var 
 		items = document.querySelectorAll('[data-draggable="item"]'), 
 		len = items.length, 
@@ -16,29 +18,43 @@
 	{
 		items[i].setAttribute('draggable', 'true');
 	}
+
 	var item = null;
-    document.addEventListener('dragstart', function(e)
+
+	document.addEventListener('dragstart', function(e)
 	{
+
 		item = e.target;
+
 		e.dataTransfer.setData('text', '');
+	
 	}, false);
+
 	document.addEventListener('dragover', function(e)
 	{
 		if(item)
 		{
 			e.preventDefault();
 		}
+	
 	}, false);	
+
 	document.addEventListener('drop', function(e)
 	{
+
 		if(e.target.getAttribute('data-draggable') == 'target')
 		{
 			e.target.appendChild(item);
-					e.preventDefault();
-		}	
+			
+			e.preventDefault();
+		}
+	
 	}, false);
+
 	document.addEventListener('dragend', function(e)
 	{
 		item = null;
+	
 	}, false);
+
 })();	
