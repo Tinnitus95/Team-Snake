622867480cb67e7144252089d82912b0f0f0b23e
Natasha <natasha.bosnjak@student.kyh.se>
2017-11-02 14:48:30 +0100

    added drag and drop for groups
    drag and drop jquery

---
+++ Snake/Javascript/dragdrop.js
@@ -0,0 +1,44 @@
+(function()
+{
+	if
+	(
+		!document.querySelectorAll 
+		|| 
+		!('draggable' in document.createElement('span')) 
+		|| 
+		window.opera
+	) 
+	{ return; }
+	for(var 
+		items = document.querySelectorAll('[data-draggable="item"]'), 
+		len = items.length, 
+		i = 0; i < len; i ++)
+	{
+		items[i].setAttribute('draggable', 'true');
+	}
+	var item = null;
+    document.addEventListener('dragstart', function(e)
+	{
+		item = e.target;
+		e.dataTransfer.setData('text', '');
+	}, false);
+	document.addEventListener('dragover', function(e)
+	{
+		if(item)
+		{
+			e.preventDefault();
+		}
+	}, false);	
+	document.addEventListener('drop', function(e)
+	{
+		if(e.target.getAttribute('data-draggable') == 'target')
+		{
+			e.target.appendChild(item);
+					e.preventDefault();
+		}	
+	}, false);
+	document.addEventListener('dragend', function(e)
+	{
+		item = null;
+	}, false);
+})();	
