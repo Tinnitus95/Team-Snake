dc9cf433261d1577179cf4a7f84df0f3d9c7aca8
Silvia02 <srj1213@gmail.com>
2017-11-05 22:20:33 +0100

    added elever närvaro

--- Snake/Javascript/attendance.js
+++ Snake/Javascript/attendance.js
@@ -2,14 +2,15 @@ function pop(div) {
 
 	document.getElementById(div).style.display = 'block';
 
-			}
+}
+
 function hide(div) {
 
 	document.getElementById(div).style.display = 'none';
-	
 
-			}
-			//To detect escape button
+
+}
+//To detect escape button
 document.onkeydown = function (evt) {
 	evt = evt || window.event;
 	if (evt.keyCode == 27) {
@@ -21,12 +22,21 @@ document.onkeydown = function (evt) {
 const code = "123";
 
 function check(form) {
-  if(form.code.value == code) {
-      document.getElementById("message").innerHTML = "Du har nu anmält närvaro till Backend 101, 09.00-12.00";
-      document.getElementById("popup").style.background = '#60ff83';
-    }
-    else {
-      document.getElementById("message").innerHTML = "Det finns ingen lektion som passar med den koden";
-      document.getElementById("popup").style.background = '#ff7a7a';
-    }
+	if (form.code.value == code) {
+		document.getElementById("message").innerHTML = "Du har nu anmält närvaro till Backend 101, 09.00-12.00";
+		document.getElementById("popup").style.background = '#60ff83';
+	} else {
+		document.getElementById("message").innerHTML = "Det finns ingen lektion som passar med den koden";
+		document.getElementById("popup").style.background = '#ff7a7a';
+	}
+}
+
+function showAttendence(id, cb) {
+	var checkbox = document.getElementById(cb);
+	var data = document.getElementById(id);
+	if (checkbox.checked === true) {
+		data.style.display = "block";
+	} else {
+		data.style.display = "none";
+	}
 }
