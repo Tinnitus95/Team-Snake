b1b932566b426a14a4d469e5c358fa610f24b076
Simonkyh <Simon.gribert@student.kyh.se>
2017-11-05 18:20:41 +0100

    Created a way for admin to create accounts

---
+++ Snake/Javascript/adminpopup.js
@@ -0,0 +1,38 @@
+function pop(div) {
+
+	document.getElementById(div).style.display = 'block';
+
+}
+function popacc() {
+
+	document.getElementById('timedacc').style.display = 'none';
+	document.getElementById('account').style.display = 'flex';
+	document.getElementById("popupacc").style.display = "flex";
+}
+
+function hide(div) {
+
+	document.getElementById(div).style.display = 'none';
+
+}
+//To detect escape button
+document.onkeydown = function (evt) {
+	evt = evt || window.event;
+	if (evt.keyCode == 27) {
+		hide('account');
+	}
+};
+function submitaccount() {
+  document.getElementById("popupacc").style.display = "none";
+  var timed = document.getElementById("timedacc");
+  timed.style.display = "flex";
+  setTimeout(function() {
+  	 document.getElementById("account").style.display = "none"
+  }, 1000);
+}
+/*document.onkeydown = function (evt) {
+	evt = evt || window.event;
+	if (evt.keyCode == 27) {
+		hide('vfeedback');
+	}
+};*/
