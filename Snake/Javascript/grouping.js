ac0a1466bdea72ba3a32c3c4b2351557b6610418
Tinnitus95 <31408430+Tinnitus95@users.noreply.github.com>
2017-11-20 10:04:39 +0100

    Added utbildning

--- Snake/Javascript/grouping.js
+++ Snake/Javascript/grouping.js
@@ -11,3 +11,12 @@ function groupGenerateGroups() {
         groupGroups.innerHTML +="<div class=\"gruppering\" id=\"grupp" + i + "\"><ol data-draggable=\"target\"><h3>Grupp " + (i+1) + "</h3></ol></div>";
     }
 }
+
+function createEdu(){
+  var eduName = document.getElementById('eduInput');
+  var name = eduName.value;
+  var eduDiv = document.getElementById("eduDivEmpty");
+  if (eduDiv !=null) {
+    eduDiv.innerHTML +="<div class=\"eduGroup\"><ol data-draggable=\"target\"><h3>" + name + "</h3></ol></div>"
+}
+}
